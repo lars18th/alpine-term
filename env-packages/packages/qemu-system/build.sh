3cdf9bdb1e760099cb9269b0c4c30394aa1c1930
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-12-17 20:43:35 +0200

    environment: update qemu configuration
    * Use /system/bin/smbd for path to smbd.
      Not used by Alpine Term but setting it anyway for
      Android paths compatibility.
    
    * Enable more supported image types.

--- env-packages/packages/qemu-system/build.sh
+++ env-packages/packages/qemu-system/build.sh
@@ -12,6 +12,9 @@ builder_step_configure() {
 		"$PACKAGE_TMPDIR"/libgcrypt-config
 	export PATH="$PATH:$PACKAGE_TMPDIR"
 
+	# Note: using --disable-stack-protector since stack protector
+	# flags already passed by build scripts but we do not want to
+	# override them with what QEMU configure provides.
 	./configure \
 		--prefix="$PACKAGE_INSTALL_PREFIX" \
 		--cross-prefix="${PACKAGE_TARGET_PLATFORM}-" \
@@ -19,30 +22,48 @@ builder_step_configure() {
 		--cc="$CC" \
 		--cxx="$CXX" \
 		--objcc="$CC" \
+		--disable-stack-protector \
+		--smbd="/system/bin/smbd" \
+		--enable-coroutine-pool \
+		--enable-trace-backends=nop \
+		--disable-guest-agent \
+		--disable-gnutls \
+		--disable-nettle \
 		--enable-gcrypt \
+		--disable-sdl \
+		--disable-sdl-image \
+		--disable-gtk \
+		--disable-vte \
 		--disable-curses \
 		--disable-iconv \
 		--enable-vnc \
+		--disable-vnc-sasl \
 		--enable-vnc-jpeg \
 		--enable-vnc-png \
+		--disable-xen \
+		--disable-xen-pci-passthrough \
+		--enable-virtfs \
+		--enable-curl \
+		--disable-fdt \
+		--disable-kvm \
+		--disable-hax \
+		--disable-hvf \
+		--disable-whpx \
+		--disable-libnfs \
+		--disable-libusb \
 		--enable-lzo \
 		--enable-snappy \
-		--enable-curl \
-		--enable-coroutine-pool \
-		--enable-virtfs \
-		--enable-trace-backends=nop \
-		--disable-hax \
-		--disable-kvm \
-		--disable-xen \
-		--disable-fdt \
-		--disable-guest-agent \
-		--disable-stack-protector \
-		--disable-bochs \
-		--disable-cloop \
-		--disable-dmg \
-		--disable-parallels \
-		--disable-qed \
-		--disable-sheepdog \
+		--disable-bzip2 \
+		--disable-lzfse \
+		--disable-seccomp \
+		--disable-libssh \
+		--disable-libxml2 \
+		--enable-bochs \
+		--enable-cloop \
+		--enable-dmg \
+		--enable-parallels \
+		--enable-qed \
+		--enable-sheepdog \
 		--target-list=x86_64-softmmu
 }
 
