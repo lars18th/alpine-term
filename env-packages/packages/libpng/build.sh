834014fa4b034b93671729b0b3c8ff6a557d6fc4
Leonid Plyushch <leonid.plyushch@gmail.com>
2020-01-19 19:51:28 +0200

    environment: switch download url for libpng

--- env-packages/packages/libpng/build.sh
+++ env-packages/packages/libpng/build.sh
@@ -1,5 +1,6 @@
 PACKAGE_VERSION="1.6.37"
-PACKAGE_SRCURL="https://downloads.sourceforge.net/sourceforge/libpng/libpng-${PACKAGE_VERSION}.tar.xz"
+#PACKAGE_SRCURL="https://downloads.sourceforge.net/sourceforge/libpng/libpng-${PACKAGE_VERSION}.tar.xz"
+PACKAGE_SRCURL="https://netcologne.dl.sourceforge.net/project/libpng/libpng16/${PACKAGE_VERSION}/libpng-${PACKAGE_VERSION}.tar.xz"
 PACKAGE_SHA256="505e70834d35383537b6491e7ae8641f1a4bed1876dbfe361201fc80868d88ca"
 PACKAGE_DEPENDS="zlib"
 
